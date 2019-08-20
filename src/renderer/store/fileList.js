bfa8dfae0f8fa623690bc9d36c898c7883e6b122
mangyui <1445327460@qq.com>
2019-08-20 09:15:34 +0800

    保存用户上次使用，调整编辑器默认不换行，及行数一一对应

--- src/renderer/store/fileList.js
+++ src/renderer/store/fileList.js
@@ -1,7 +1,7 @@
 export default {
   state: {
-    fileList: JSON.parse(sessionStorage.getItem('fileList')) || [],
-    len: parseInt(sessionStorage.getItem('len')) || 0
+    fileList: JSON.parse(localStorage.getItem('fileList')) || [],
+    len: parseInt(localStorage.getItem('len')) || 0
   },
   mutations: { // 无异步
     addFlie (state, file) {
@@ -15,16 +15,16 @@ export default {
         ischange: false
       }
       state.fileList.push(fe)
-      sessionStorage.setItem('len', JSON.stringify(state.len))
-      sessionStorage.setItem('fileList', JSON.stringify(state.fileList))
+      localStorage.setItem('len', JSON.stringify(state.len))
+      localStorage.setItem('fileList', JSON.stringify(state.fileList))
     },
     removeFile (state, index) {
       state.fileList.splice(index, 1)
-      sessionStorage.setItem('fileList', JSON.stringify(state.fileList))
+      localStorage.setItem('fileList', JSON.stringify(state.fileList))
     },
     updateFile (state, index, file) {
       state.fileList[index].file = file
-      sessionStorage.setItem('fileList', JSON.stringify(state.fileList))
+      localStorage.setItem('fileList', JSON.stringify(state.fileList))
     },
     updateName (state, value) {
       for (let j = 0, len = state.fileList.length; j < len; j++) {
@@ -34,7 +34,7 @@ export default {
           break
         }
       }
-      sessionStorage.setItem('fileList', JSON.stringify(state.fileList))
+      localStorage.setItem('fileList', JSON.stringify(state.fileList))
     },
     updatePath (state, value) {
       for (let j = 0, len = state.fileList.length; j < len; j++) {
@@ -44,7 +44,7 @@ export default {
           break
         }
       }
-      sessionStorage.setItem('fileList', JSON.stringify(state.fileList))
+      localStorage.setItem('fileList', JSON.stringify(state.fileList))
     },
     updateContent (state, value) {
       for (let j = 0, len = state.fileList.length; j < len; j++) {
@@ -54,11 +54,11 @@ export default {
           break
         }
       }
-      sessionStorage.setItem('fileList', JSON.stringify(state.fileList))
+      localStorage.setItem('fileList', JSON.stringify(state.fileList))
     },
     removeAll (state) {
       state.fileList = []
-      sessionStorage.setItem('fileList', JSON.stringify(state.fileList))
+      localStorage.setItem('fileList', JSON.stringify(state.fileList))
     }
   },
   actions: { // 可异步
